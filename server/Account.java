994e2a13fc4a25814ddbe78f2f7cd9729d6bb1d1
Spyridon Giannakopoulos <spyrosgian7@gmail.com>
2021-11-25 11:34:31 +0000

    Replacing accountName with accountType in Account class. Creating a new getter

--- server/Account.java
+++ server/Account.java
@@ -3,10 +3,13 @@ package newbank.server;
 public class Account {
 
 	private String accountName;
+	enum AccountType {MAIN, SAVINGS, INVESTMENTS};
+	private final Account.AccountType accountType;
 	private double balance;
 
-	public Account(String accountName, double balance) {
+	public Account(AccountType accountType, double balance) {
 		this.accountName = accountName;
+		this.accountType = accountType;
 		this.balance = balance;
 	}
 
@@ -18,6 +21,10 @@ public class Account {
 		return accountName;
 	}
 
+	public Account.AccountType getAccountType() {
+		return accountType;
+	}
+
 	public double getBalance() {
 		return balance;
 	}
@@ -25,6 +32,5 @@ public class Account {
 	public void setBalance(double d) {
 		balance = d;
 	}
-
 }
 
